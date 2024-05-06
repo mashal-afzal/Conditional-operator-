51651751afdba17d3f6211c132593fa849dfce45
Mashal Afzal <mashal.afzal2996@gmail.com>
2024-05-06 00:16:15 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+"use strict";
+let percentage = 88;
+if (percentage >= 80) {
+    console.log("Your grade is A1");
+}
+else if (percentage == 79 && percentage >= 70) {
+    console.log("your grade is A");
+}
+else if (percentage == 69 && percentage >= 60) {
+    console.log("Your grade is B");
+}
+else if (percentage == 59 && percentage >= 50) {
+    console.log("Your grade is C");
+}
+else if (percentage < 50 && percentage >= 40) {
+    console.log("Your grade is D");
+}
+if (percentage < 40) {
+    console.log("You're fail");
+}
